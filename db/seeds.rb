64a10440b1e967aca0ec45cde1e660d1ba6341f2
Leah Phan <leah.phan@gmail.com>
2019-03-29 18:23:18 -0400

    script to seed the user data from JSON file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+# Load Users
+user_file = File.read(File.join(File.dirname(__FILE__), 'users.json'))
+parsed_users = JSON.parse(user_file)
+
+parsed_users.each do |user, |
+  User.create(
+    index: user["index"],
+    name: user["name"],
+    gender: user["gender"],
+    age: user["age"],
+    email: user["email"],
+    phone: user["phone"],
+    address: user["address"],
+    about: user["about"]
+  )
+end
+
